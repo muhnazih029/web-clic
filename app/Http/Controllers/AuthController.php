4d53c29463c4a1d8ce0b854616cc84fd02b23e27
bal16 <ahmadhamidbalya@gmail.com>
2024-06-30 19:37:36 +0700

    add auth and register controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function index()
+    {
+        //login view
+    }
+    public function create()
+    {
+        //login logic
+    }
+    public function destroy()
+    {
+        //logout logic
+    }
+
+}
