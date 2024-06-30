a9156771fee64e87d197017a1f73c32be2e34e26
bal16 <ahmadhamidbalya@gmail.com>
2024-06-30 12:17:54 +0700

    fixing title issue and minor improve

--- resources/views/helloworld.blade.php
+++ resources/views/helloworld.blade.php
@@ -1,4 +1,4 @@
-<x-layout>
+<x-layout title="HelloWorld">
     <div class="text-center w-full h-screen flex justify-center items-center">
         <div class="h-12 hover:bg-black hover:text-white transition-colors ease-in-out duration-500">
             <h1 class="text-5xl hover:bg-black hover:text-white transition-all duration-300">Hello World</h1>
