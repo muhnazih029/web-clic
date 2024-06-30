f0c578341669ce1583254649df8323471dd3681a
bal16 <ahmadhamidbalya@gmail.com>
2024-06-30 13:10:17 +0700

    add debugbar

--- resources/views/helloworld.blade.php
+++ resources/views/helloworld.blade.php
@@ -1,4 +1,4 @@
-<x-layout title="HelloWorld">
+<x-layout title="Hello World">
     <div class="text-center w-full h-screen flex justify-center items-center">
         <div class="relative group">
             <div class="-z-30 absolute left-0 bg-black h-14 w-0 group-hover:w-full transition-all duration-500"></div>
