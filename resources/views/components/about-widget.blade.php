2c4859f958ddc2b2b41adf48e24b85e6a255f002
bal16 <ahmadhamidbalya@gmail.com>
2024-07-09 20:06:50 +0700

    adj(about widget): change 'about widgets' to functional components

---
+++ resources/views/components/about-widget.blade.php
@@ -0,0 +1,6 @@
+<div class="flex flex-col items-center px-5 ">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="size-16 text-primary">
+        <use href="#{{ strtolower($text) }}" />
+    </svg>
+    <p>{{ $text }}</p>
+</div>
